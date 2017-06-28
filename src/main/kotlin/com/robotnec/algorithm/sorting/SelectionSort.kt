46c7718c3d024c764e64b6da62c6fe998f21b72e
Kyrylo Zapylaiev <zapylaev@gmail.com>
2017-06-29 01:31:36 +0300

    Improve selection sort

--- src/main/kotlin/com/robotnec/algorithm/sorting/SelectionSort.kt
+++ src/main/kotlin/com/robotnec/algorithm/sorting/SelectionSort.kt
@@ -9,16 +9,14 @@ class SelectionSort: Sorting {
             return input
         }
         for (i in 0..input.size - 1) {
-            var biggest = input[0]
             var biggestIndex = 0
             for (j in 1..input.size - 1 - i) {
-                if (input[j] > biggest) {
-                    biggest = input[j]
+                if (input[j] > input[biggestIndex]) {
                     biggestIndex = j
                 }
             }
             val temp = input[input.size - 1 - i]
-            input[input.size - 1 - i] = biggest
+            input[input.size - 1 - i] = input[biggestIndex]
             input[biggestIndex] = temp
         }
         return input
